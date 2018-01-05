50b57f7795a14b3309aa677dc2f09043e5b24b0e
Emmanuel Kimmerlin <emmanuel.kimmerlin@thomsonreuters.com>
2018-01-05 21:17:38 +0100

    Add potential tests

--- test/documentTest.js
+++ test/documentTest.js
@@ -20,8 +20,23 @@ let xmlString =
 '  </person>' + 
 '</people>';
 
+let xmlNSString = 
+'<?xmlString version="1.0"?>' + 
+'<people xmlns:xul = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" >' + 
+'  <person>' + 
+'	<name first="george" last="bush" />' + 
+'	<address street="1600 pennsylvania avenue" city="washington" country="usa"/>' + 
+'	<phoneNumber>202-456-1111</phoneNumber>' + 
+'  </person>' + 
+'  <person>' + 
+'	<name first="tony" last="blair" />' + 
+'	<address street="10 downing street" city="london" country="uk"/>' + 
+'	<phoneNumber>020 7925 0918</phoneNumber>' + 
+'  </person>' + 
+'</people>';
+
 describe('xpath', () => {
-  it('xmldom evaluate', (done) => {
+  it('xmldom evaluate xml', (done) => {
     let dom = new XMLFeatures.DOMParser();
     let xmlDoc = dom.parseFromString(xmlString, 'text/xmlString');
     var personIterator = xmlDoc.evaluate('//person', xmlDoc, null, XMLFeatures.XPathResult.ANY_TYPE, null );
@@ -30,5 +45,24 @@ describe('xpath', () => {
     }
     done();
   });
+  // it('xmldom evaluate NS xml without NSResolver failed', (done) => {
+  //   let dom = new XMLFeatures.DOMParser();
+  //   let xmlDoc = dom.parseFromString(xmlNSString, 'text/xmlString');
+  //   var personIterator = xmlDoc.evaluate('//person', xmlDoc, null, XMLFeatures.XPathResult.ANY_TYPE, null );
+  //   let person;
+  //   while (person = personIterator.iterateNext()) {
+  //   }
+  //   done();
+  // });
+  // it('xmldom evaluate NS xml with NSResolver succeeded', (done) => {
+  //   let dom = new XMLFeatures.DOMParser();
+  //   let xmlDoc = dom.parseFromString(xmlNSString, 'text/xmlString');
+  //   var nsResolver = xmlDoc.createNSResolver( xmlDoc.ownerDocument == null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
+  //   var personIterator = xmlDoc.evaluate('//person', xmlDoc, nsResolver, XMLFeatures.XPathResult.ANY_TYPE, null );
+  //   let person;
+  //   while (person = personIterator.iterateNext()) {
+  //   }
+  //   done();
+  // });
 });
 
